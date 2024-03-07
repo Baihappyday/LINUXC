760d15f3f77319df37aff0ae0717189a02c081be
Baihappyday <2098704205@qq.com>
2024-03-07 15:50:43 +0800

    0307

--- 02fopen.cpp
+++ 02fopen.cpp
@@ -1,18 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <errno.h>
-
+#include<string.h>
 int main()
 {
     FILE *fp;
     fp = fopen("lin","r");
     if(fp == NULL)
     {
-        fprintf(stderr, "fopen() failed errno = %d\n", errno);
+        
+//	fprintf(stderr, "fopen() failed errno = %d\n", errno);
+//	perror("fopen()");
+	fprintf(stderr,"fopen():%s\n",strerror(errno));
         exit(1);
     }
     puts("ok!");
-
     exit(0);
     return 0;
 }
---
+++ 03gitignore.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("ignore 无后缀 success");
+    return 0;
+}
